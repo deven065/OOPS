c769a0edb59ca27545990e9159bb75c3f2883ec0
deven065 <devenrikame55@gmail.com>
2026-07-05 16:48:59 +0530

    Singleton class

---
+++ src/packages/inheritence/BoxWeight.java
@@ -0,0 +1,28 @@
+package packages.inheritence;
+
+public class BoxWeight extends Box{
+    double weight;
+
+    BoxWeight() {
+        this.weight = -1;
+    }
+
+    BoxWeight(BoxWeight other) {
+        super(other);
+        weight = other.weight;
+    }
+
+    BoxWeight(double side, double weight) {
+        super(side);
+        this.weight = weight;
+    }
+
+    public BoxWeight(double l, double h, double w, double weight) {
+        super(l, h, w); //  what is this? call the parent class constructor
+        //  used to initialise values present in parent class
+
+//        System.out.println(super.weight);
+
+        this.weight = weight;
+    }
+}
---
+++ src/packages/inheritence/Main.java
@@ -0,0 +1,29 @@
+package packages.inheritence;
+
+public class Main {
+    static void main() {
+//        Box box1 = new Box(4.6, 7.9, 9.9);
+//        Box box2 = new Box(box1);
+//
+//        System.out.println(box1.l + " " + box1.w + " " + box1.h);
+//        System.out.println(box2.l + " " + box2.w + " " + box2.h);
+
+//        BoxWeight box3 = new BoxWeight();
+//        BoxWeight box4 = new BoxWeight(2, 3, 4, 8);
+//        System.out.println(box3.h + " " + box3.weight);
+
+//        Box box5 = new  BoxWeight(2, 3, 4, 8);
+//        System.out.println(box5.w);
+
+        //  there are many variables in both parent and child classes
+        //  you are given access to variables that are in the ref type i.e. BoxWeight
+        //  hence you should have access to weight variable
+        //  this alo means, that the ones you are trying to access should be initialized
+        //  but here, when the obj itself is of type parent class, how will you call the constructor of child class
+
+//        BoxWeight box6 = new Box(2, 3, 4);
+//        System.out.println(box6);
+
+        BoxPrice box = new BoxPrice(5, 8, 200);
+    }
+}
